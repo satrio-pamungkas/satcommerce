29c4a7628352e66d38d9490baf544b14ef7a3679
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-12-30 22:30:38 +0700

    feat: added request body validation

--- SatCommercePostgreSQL/Services/Payment/PaymentCommandAPI/Schemas/MakePaymentRequest.cs
+++ SatCommercePostgreSQL/Services/Payment/PaymentCommandAPI/Schemas/MakePaymentRequest.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PaymentCommandAPI.Schemas;
 
 public class MakePaymentRequest
 {
+    [Required]
     public Guid CartId { get; set; }
+    [Required]
     public Guid PaymentId { get; set; }
 }
