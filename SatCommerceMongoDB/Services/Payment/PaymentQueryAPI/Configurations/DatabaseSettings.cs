790654672711e49cbfe773da0a23321bf6d90b10
Muhammad Raihan Satrio <adewipo@gmail.com>
2023-01-03 11:33:31 +0700

    feat: added PaymentQueryAPI MongoDB

---
+++ SatCommerceMongoDB/Services/Payment/PaymentQueryAPI/Configurations/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+namespace PaymentQueryAPI.Configurations;
+
+public class DatabaseSettings
+{
+    public String ConnectionString { get; set; } = null!;
+    public String DatabaseName { get; set; } = null!;
+    public String CollectionName { get; set; } = null!;
+}
---
+++ SatCommerceMongoDB/Services/Payment/PaymentQueryAPI/Handlers/PaymentHandler.cs
@@ -0,0 +1,26 @@
+using System.Text.Json;
+using PaymentQueryAPI.Models;
+using PaymentQueryAPI.Repositories;
+
+namespace PaymentQueryAPI.Handlers;
+
+public class PaymentHandler : IPaymentHandler
+{
+    private readonly IPaymentRepository _paymentRepository;
+
+    public PaymentHandler(IPaymentRepository paymentRepository)
+    {
+        this._paymentRepository = paymentRepository;
+    }
+
+    public void CreatePayment(string data)
+    {
+        List<Payment>? payments = JsonSerializer.Deserialize<List<Payment>>(data);
+        this._paymentRepository.Create(payments!);
+    }
+
+    public void DeletePayment()
+    {
+        this._paymentRepository.DeleteAll();
+    }
+}
